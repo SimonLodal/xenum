d0cc7b7738b09e02d82b1f964476791c93549d6a
Simon Lodal <simon.lodal@gmail.com>
2018-07-12 18:47:26 +0200

    Also split source files for testing xenums in three.

---
+++ xenum/test/src/xenums/P0combs.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * @author Simon Lodal
+ * @copyright 2018 Simon Lodal <simonl@parknet.dk>
+ * @license GNU GPL version 3
+ */
+
+#include <test/xenum/xenums/P0combs.hpp>
+
+XENUM5_DEFINE(P0combs)
+
--- xenum/test/src/xenums/P1combs.cpp
+++ xenum/test/src/xenums/P1combs.cpp
@@ -5,11 +5,7 @@
  * @license GNU GPL version 3
  */
 
-#include <test/xenum/xenums/P0combs.hpp>
 #include <test/xenum/xenums/P1combs.hpp>
-#include <test/xenum/xenums/P2combs.hpp>
 
-XENUM5_DEFINE(P0combs)
 XENUM5_DEFINE(P1combs)
-XENUM5_DEFINE(P2combs)
 
